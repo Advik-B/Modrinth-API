44d24600f5e6d2d5be1168556553ea6ef34d9846
Advik <advik.b@gmail.com>
2023-01-25 22:31:12 +0530

    Update __main__.py

--- modrinth/__main__.py
+++ modrinth/__main__.py
@@ -2,8 +2,8 @@ from .base import ModrinthClient
 from pprint import pprint
 
 client = ModrinthClient(cache=True)
-mods = client.fetch("search", {"query": "Sodium"})
 
-for mod in mods:
-    pprint(mod["title"])
-    print("." * 80)
+print("Fetching project...")
+project = client.get_project("iris")
+for img in project.gallery:
+    print(img.url)
